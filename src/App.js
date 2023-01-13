0539fe7123048681f3763bb3c2974fd46d8edcaa
DroidekaMuSo <diego.munoz_solis@hotmail.com>
2023-01-12 20:29:04 -0600

    Reestableciendo proyecto

--- src/App.js
+++ src/App.js
@@ -1,47 +1,9 @@
+import React from 'react'
 
-import ItemDetailContainer from "./components/itemDetailContainer/ItemDetailContainer";
-import ItemListContainer from "./components/itemListContainer/ItemListContainer";
-import Navbar from "./components/navBar/NavBar"
-
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Cart from "./components/cart/Cart";
-
-import CartContextProvider from "./context/CartContext";
-import Form from "./components/form/Form";
-
-
-function App() {
-
-  // CREAR EL ENRUTADO
-
+const App = () => {
   return (
-   
-    <BrowserRouter>
-      <CartContextProvider>
-
-        <Navbar />
-
-        <Routes>
-
-          <Route path="/" element={<ItemListContainer />} />
-          
-          <Route path="/category/:categoryName" element={<ItemListContainer />} />
-
-          <Route path="/itemDetail/:id" element={ <ItemDetailContainer /> } />
-
-          <Route path="/checkout" element={ <Form /> } />
-
-          <Route path="/cart" element={ <Cart /> } />
-
-          <Route path="*" element={ <h2>Lo siento esta url no existe</h2> } />
-
-        </Routes>
-
-      </CartContextProvider>
-    </BrowserRouter>
-
-  );
-  
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
